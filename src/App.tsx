c45148d17adb962522f166aa54972172168de77c
kamil.danielski@gmail.com <kamil.danielski@gmail.com>
2022-04-06 20:55:59 +0200

    Git init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar/navbar'
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = () => {
+    return(
+        <nav>
+            <div>Home</div>
+            <div>Explore</div>
+            <div>Notifications</div>
+            <div>Messages</div>
+            <div>Bookmarks</div>
+            <div>Lists</div>
+            <div>Profile</div>
+            <div>More</div>
+            
+        </nav>
+    )
+}
+
+export default Navbar
