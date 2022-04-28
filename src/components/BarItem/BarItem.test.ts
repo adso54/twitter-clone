43ea613dd45079d8a75182850caa0d0ec4d2e8bb
kamil.danielski@gmail.com <kamil.danielski@gmail.com>
2022-04-28 21:16:06 +0200

    Config tests

---
+++ src/components/BarItem/BarItem.test.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import BarItem from './index'
+import {HouseIcon} from '../../assets/images'
+
+test('Rendered BarItem match the snapshot', async () => {
+    // const { getByText } = render(<BarItem displayText='Home' Icon = {HouseIcon} />)
+    expect(true).toBe(true)
+})
