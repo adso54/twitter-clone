c37169777f790e2c45662a75766a3735223ffee0
kamil.danielski@gmail.com <kamil.danielski@gmail.com>
2022-04-29 06:28:24 +0200

    First working react test

--- src/components/BarItem/BarItem.test.ts
+++ src/components/BarItem/BarItem.test.ts
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-
+import '@testing-library/jest-dom'
 import BarItem from './index'
 import {HouseIcon} from '../../assets/images'
 
 test('Rendered BarItem match the snapshot', async () => {
-    // const { getByText } = render(<BarItem displayText='Home' Icon = {HouseIcon} />)
-    expect(true).toBe(true)
+    const component = render(BarItem({displayText: 'TEST', Icon: HouseIcon}))
+    expect(component.getByText('TEST')).toBeInTheDocument()
+    expect(component).toMatchSnapshot()
 })
